78f7c45dd877f20632e08fc76b5bf5d6d78e64c3
ThatCodebyTony <twu1415@gmail.com>
2024-11-03 19:20:40 -0800

    let messageEl = document.getElementById("message-el");

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-let firstCard = 2
+let firstCard = 10
 let secondCard = 11
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
 
+let messageEl = document.getElementById("message-el");
+
 function startGame() {
     if (sum < 21) {
-        message = "Do you want to draw a new card? 👍"
+        message = "Do you want to draw a new card?"
     } else if (sum === 21) {
         hasBlackJack = true
-        message = "Whoo! You've got Blackjack! 💕"
+        message = "Whoo! You've got Blackjack!"
     } else {
-        message = "You're out of the game! 🤷‍♂️"
+        message = "You're out of the game!"
         isAlive = false
     }
+    messageEl.textContent = message
 };
