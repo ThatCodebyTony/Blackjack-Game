ad5f6a682169d650eea2318f81ad4633986c6abe
ThatCodebyTony <twu1415@gmail.com>
2024-11-03 19:27:56 -0800

    trying out querySelector

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ let isAlive = true
 let message = ""
 
 let messageEl = document.getElementById("message-el");
-let sumEl = document.getElementById("sum-el");
+// let sumEl = document.getElementById("sum-el");
+let sumEl = document.querySelector("#sum-el");
 
 function startGame() {
     if (sum < 21) {
